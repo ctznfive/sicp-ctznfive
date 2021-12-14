064decbcf9bd17cd8b7843317fd3527243995a36
ctznfive <ctznfive@gmail.com>
2021-12-14 16:34:16 +0300

    Updated test-cases for exercise 1.8

--- tests/1.8.test.rkt
+++ tests/1.8.test.rkt
@@ -4,7 +4,8 @@
            rackunit/text-ui
            "../exercises/1.8.rkt")
 
-(define cube-root-test  (test-suite
+(define cube-root-test
+  (test-suite
    "exercise 1.8"
               
    (test-case
@@ -22,11 +23,10 @@
     (check-= (cube-root 1e-7) 0.00464 1e-5)
     (check-= (cube-root 1e-300) 1e-100 1e-10))
 
+   (test-case
     "for negative numbers"
     (check-= (cube-root -1728) -12.0 1e-10)
     (check-= (cube-root -1e300) -1e100 1e-10)
-    (check-= (cube-root -1e-300) -1e-100 1e-10)))
+    (check-= (cube-root -1e-300) -1e-100 1e-10))))
 
 (run-tests cube-root-test 'verbose)
-
-
